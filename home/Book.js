4fa715dd8284ab1084daa11bb753561df22bb433
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-01-13 18:10:40 +0100

    Icons

---
+++ home/Book.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Book = (props) => {
+    return (
+        <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <polygon points="0 0 24 0 24 24 0 24" />
+                <mask fill="white">
+                    <use />
+                </mask>
+                <use fill="currentColor" fill-rule="nonzero" />
+            </g>
+        </svg>
+    )
+}
+
+export default Book
